a4d606f241b594d7c31d1c2c21a3d876718e3d73
Pooja Chaudhary <pooja.chaudhary0080@gmail.com>
2021-07-16 15:49:31 +0530

    update code

--- src/App.js
+++ src/App.js
@@ -1,19 +1,10 @@
 import React from 'react';
 import Todo from './component/Todo';
-import ApiData from './component/ApiData';
+
 
 const App = () => {
     return (
-        <div className="container-fluid">
-            <div className="row">
-//                 <div className="col-sm-6">
-//                     <Todo />
-//                 </div>
-                <div className="col-sm-12">
-                    <ApiData />
-                </div>
-            </div>
-        </div>
+        <Todo />   
     );
 };
 
